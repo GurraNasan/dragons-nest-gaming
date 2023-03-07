5a3f7e99c8310f66b98b5e4da4bbcd40d0ff64e7
GurraNasan <gurra.naslund@gmail.com>
2023-03-07 15:29:08 +0000

    add produkt app, set up category and product models, filled the databas with data, made some small changen to main navbar

---
+++ products/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+# Borrowed code from the boutiq ado project in the Code institute course
+class Category(models.Model):
+    #main category, gametype, merch
+    name = models.CharField(max_length=200, unique=True)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class SubCategory(models.Model):
+    # Models for deals or new arrivals
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name='category1')
+    category2 = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name='category2')
+    category3 = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name='category3')
+    sub_category = models.ForeignKey('SubCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=240)
+    sku = models.CharField(max_length=240, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    available = models.DecimalField(max_digits=3, decimal_places=0)
+    new_arrival = models.BooleanField(default=False, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
