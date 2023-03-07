ec1b73faad18d6d99fb4d5c22e1f10d69aed79e1
GurraNasan <gurra.naslund@gmail.com>
2023-03-07 16:30:13 +0000

    add base template for product and set up views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def products_list(request):
+    """ 
+        A view for all products and functionality for search and filtering by categories.
+        insperied by Boutique Ado project in Code institute course
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products' : products,
+    }
+    
+    return render(request, 'products/products.html', context)
