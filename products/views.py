a340dc0db01992e03af45a2e0d8bb1cd9250986d
GurraNasan <gurra.naslund@gmail.com>
2023-03-08 22:34:18 +0000

    fixed the search function

--- products/views.py
+++ products/views.py
@@ -1,23 +1,37 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 def products_list(request):
-    """ 
+    """
         A view for all products and functionality for search and filtering by categories.
         insperied by Boutique Ado project in Code institute course
     """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "I think you forgot to write anything, TRY AGAIN!")
+                return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_info(request, product_id):
-    """ 
+    """
         A view for a singel product info
     """
     product = get_object_or_404(Product, pk=product_id)
