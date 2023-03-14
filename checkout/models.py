9f673967a77346bf6a14734273629a7fe120c60e
GurraNasan <gurra.naslund@gmail.com>
2023-03-14 08:23:30 +0000

    migrate the checkout models and set up function for update grand total

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,11 @@
 import uuid
 
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 from django_countries.fields import CountryField
+
+from products.models import Product
 
 
 class Order(models.Model):
@@ -22,3 +26,37 @@ class Order(models.Model):
     def _generate_order_number(self):
         """ Generate a random unique order number using UUID """
         return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = settings.STANDARD_DELIVERY
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
