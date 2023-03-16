88e46624166785f61e08bb2ed85653baae11e7d3
GurraNasan <gurra.naslund@gmail.com>
2023-03-16 16:38:34 +0000

    add rating to admin page

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+
+class RatingAdmin(admin.ModelAdmin):
+
+    list_display = ( 
+        'product',
+        'user_profile',
+        'rating',
+        'review_title',
+    )
+
+
+admin.site.register(Rating, RatingAdmin)
