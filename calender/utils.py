8c7f67053d3928f903b83080d3c4d8f67a71f8f1
GurraNasan <gurra.naslund@gmail.com>
2023-03-18 10:57:44 +0000

    migrate event model, add calender view and function to render the calender

--- calender/utils.py
+++ calender/utils.py
@@ -1,4 +1,4 @@
-from datatime import datatime, timedelta
+from datetime import datetime, timedelta
 from calendar import HTMLCalendar
 from .models import Event
 
--- calender/views.py
+++ calender/views.py
@@ -1,6 +1,32 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.utils.safestring import mark_safe
+
+from .models import Event
+from .utils import create_calender
 
 
-def calender(request):
+class calender(ListView):
     """ A view for the calender"""
-    return render(request, 'calender/calender.html')
+    model = Event
+    template_name = 'calender/calender.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        date = get_date(self.request.GET.get('day', None))
+
+        cal = create_calender(date.year, date.month)
+
+        html_cal = cal.formatmonth(withyear=True)
+
+        context['calender'] = mark_safe(html_cal)
+        return context
+
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(d) in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
