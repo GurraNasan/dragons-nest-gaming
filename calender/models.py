b45c4e3b77f8a84712592ed40eddf226ffb1a24e
GurraNasan <gurra.naslund@gmail.com>
2023-03-18 09:57:17 +0000

    write function to make calender as a table

--- calender/models.py
+++ calender/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+        A model for the events to the calaneder
+    """
+    title = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField()
+    start_time = models.DateTimeField(null=False, blank=False)
+    end_time = models.DateTimeField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
