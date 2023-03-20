0d6547897b8b3309353a706ee6e70a836d0bcfc5
GurraNasan <gurra.naslund@gmail.com>
2023-03-20 13:53:42 +0000

    add policy content, fixed problem with database

--- dragons_nest/settings.py
+++ dragons_nest/settings.py
@@ -128,6 +128,8 @@ WSGI_APPLICATION = 'dragons_nest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
@@ -135,8 +137,8 @@ if 'DATABASE_URL' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
