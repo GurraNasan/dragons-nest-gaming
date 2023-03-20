b0b98c76c65a07e60a983f5cfae7cdfd5208cd03
GurraNasan <gurra.naslund@gmail.com>
2023-03-20 15:12:47 +0000

    add robot.txt and sitemap, add address to store in footer and link to googlemap

--- home/views.py
+++ home/views.py
@@ -8,19 +8,14 @@ def index(request):
 
 def privacy(request):
     """ A view for the privacy policy"""
-    return render(request, 'home/index.html')
+    return render(request, 'home/privacy_policy.html')
 
 
 def return_and_refund(request):
     """ A view for the return and refund policy"""
-    return render(request, 'home/index.html')
-
-
-def shipping(request):
-    """ A view for the shipping policy"""
-    return render(request, 'home/index.html')
+    return render(request, 'home/return_and_refund_policy.html')
 
 
 def terms(request):
     """ A view for the term and conditions"""
-    return render(request, 'home/index.html')
+    return render(request, 'home/term_and_conditions.html')
