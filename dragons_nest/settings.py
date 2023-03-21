6a4e7b596d93956fa0badb8424ca48b2a1a0b406
GurraNasan <gurra.naslund@gmail.com>
2023-03-21 16:03:14 +0000

    fixed email host password

--- dragons_nest/settings.py
+++ dragons_nest/settings.py
@@ -221,11 +221,11 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'dragonsnestgamingas@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLE = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Default primary key field type
