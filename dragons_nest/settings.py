738edda53f39e7931bbce80388cc375a1b8c0436
GurraNasan <gurra.naslund@gmail.com>
2023-03-21 21:58:41 +0000

    fix webhook

--- dragons_nest/settings.py
+++ dragons_nest/settings.py
@@ -210,11 +210,11 @@ if 'USE_AWS' in os.environ:
 
 # stripe
 FREE_DELIVERY_THRESHOLD = 500
-STANDARD_DELIVERY_PRECENTAGE = 50
+STANDARD_DELIVERY_PRECENTAGE = 20
 STRIPE_CURRENCY = 'nok'
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
